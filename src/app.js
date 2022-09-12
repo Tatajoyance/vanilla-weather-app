68826826834a841f2cfdbf75700ddd783dd5f995
Tetiana Shcherbina <tatajoyance@gmail.com>
2022-09-12 19:20:54 +0100

    minor update
    still see no problem, but 4 columns are displayed

--- src/app.js
+++ src/app.js
@@ -17,24 +17,27 @@ function displayForecast() {
   let forecastElement = document.querySelector("#forecast");
 
   let days = ["Thu", "Fri", "Sat", "Sun"];
+
   let forecastHTML = `<div class="row">`;
   days.forEach(function (day) {
     forecastHTML =
       forecastHTML +
       `
-              <div class="col-2">
-                <div class="weather-forecast-date">${day}</div>
-                <img
-                  src="http://openweathermap.org/img/wn/50d@2x.png"
-                  alt=""
-                  width="42"
-                />
-                <div class="weather-forecast-temperature">18°</div>
-              </div>
-            </div>`;
+      <div class="col-2">
+        <div class="weather-forecast-date">${day}</div>
+        <img
+          src="http://openweathermap.org/img/wn/50d@2x.png"
+          alt=""
+          width="42"
+        />
+        <div class="weather-forecast-temperatures">
+          <div class="weather-forecast-temperature">18°</div>
+        </div>
+      </div>
+  `;
   });
 
-  forecastHMTL = forecastHTML + `</div>`;
+  forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
   console.log(forecastHTML);
 }
