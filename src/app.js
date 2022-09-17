b3908e39bc53b2845edbce3fb53cd316889ce02d
Tetiana Shcherbina <tatajoyance@gmail.com>
2022-09-17 16:13:14 +0100

    removed Fahrenheit

--- src/app.js
+++ src/app.js
@@ -108,35 +108,35 @@ function handleSubmit(event) {
   console.log(cityInputElement.value);
 }
 
-function displayFahrenheitTemperature(event) {
-  event.preventDefault();
-  //remove the active class the celsius link//
-  let temperatureElement = document.querySelector("#temperature");
-  celsiusLink.classList.remove("active");
-  fahrenheitLink.classList.add("active");
-  let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
-  //alert(fahrenheitTemperature);
-  temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
-}
+//function displayFahrenheitTemperature(event) {
+// event.preventDefault();
+//remove the active class the celsius link//
+// let temperatureElement = document.querySelector("#temperature");
+//celsiusLink.classList.remove("active");
+//fahrenheitLink.classList.add("active");
+//let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
+//alert(fahrenheitTemperature);
+//// temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
+//}
 
-function displayCelsiusTemperature(event) {
-  event.preventDefault();
-  celsiusLink.classList.add("active");
-  fahrenheitLink.classList.remove("active");
-  let temperatureElement = document.querySelector("#temperature");
-  temperatureElement.innerHTML = Math.round(celsiusTemperature);
-}
+//function displayCelsiusTemperature(event) {
+//event.preventDefault();
+//celsiusLink.classList.add("active");
+//fahrenheitLink.classList.remove("active");
+//let temperatureElement = document.querySelector("#temperature");
+//temperatureElement.innerHTML = Math.round(celsiusTemperature);
+//}
 
-let celsiusTemperature = null;
+//let celsiusTemperature = null;
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
 
-let fahrenheitLink = document.querySelector("#fahrenheit-link");
-fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
+//let fahrenheitLink = document.querySelector("#fahrenheit-link");
+//fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 
-let celsiusLink = document.querySelector("#celsius-link");
-celsiusLink.addEventListener("click", displayCelsiusTemperature);
+//let celsiusLink = document.querySelector("#celsius-link");
+//celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("Kyiv");
 
