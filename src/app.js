294a24e78972197ce1d9a109de92b263daa67017
Tetiana Shcherbina <tatajoyance@gmail.com>
2022-09-17 15:05:37 +0100

    added one call api

--- src/app.js
+++ src/app.js
@@ -13,7 +13,8 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
 
   let days = ["Thu", "Fri", "Sat", "Sun"];
@@ -42,8 +43,15 @@ function displayForecast() {
   console.log(forecastHTML);
 }
 
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "3bc520cc14bbdedfd7e45158f2ef0439";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  console.log(apiUrl);
+  axios.get(apiUrl).then(displayForecast);
+}
+
 function displayTemperature(response) {
-  console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
@@ -67,10 +75,12 @@ function displayTemperature(response) {
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
   tempLikeElement.innerHTML = Math.round(response.data.main.feels_like);
+
+  getForecast(response.data.coord);
 }
 
 function search(city) {
-  let apiKey = "6d01eef1b1c1dd92545f51cab940e040";
+  let apiKey = "3bc520cc14bbdedfd7e45158f2ef0439";
   //let city = "Kyiv";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayTemperature);
@@ -114,7 +124,6 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("Kyiv");
-displayForecast();
 
 //air pollution data//
 //function airpollutionElement(response) {
